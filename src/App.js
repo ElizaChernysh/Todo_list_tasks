7da28f3f2dac285af801690b0eef936d931f7c4d
Elizaveta <elizacalvador@gmail.com>
2022-05-16 21:42:51 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './App.scss';
+import { Header } from './components/Header/Header';
+import { AppTodo } from './components/AppTodo/AppTodo';
+import { ListTodo } from './components/ListTodo/ListTodo';
+import { useState } from 'react';
+import { Container } from 'react-bootstrap';
 
 function App() {
+
+  const [todo, setTodo] = useState([
+    {
+      id: 1,
+      title: 'go to the store',
+      status: true
+    },
+    {
+      id: 2,
+      title: 'do housework',
+      status: true
+    },
+    {
+      id: 3,
+      title: 'do homework',
+      status: false
+    },
+
+  ]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container>
+        <Header />
+        <AppTodo todo={todo} setTodo={setTodo} />
+        <ListTodo todo={todo} setTodo={setTodo} />
+      </Container>
     </div>
   );
 }
